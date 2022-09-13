71b432f635f43788cf341abbdd53f8ce9dcd37c7
projectperfect <jprfct@protonmail.com>
2022-09-12 23:45:02 -0500

    get image from github profile

--- ui.js
+++ ui.js
@@ -4,6 +4,18 @@ class UI {
     }
 
     showProfile(user) {
-        
+        this.profile.innerHTML = `
+            <div calss="card card-body mb-3">
+                <div class="row>
+                    <div class="col-md-3">
+                        <img class="img-fluid mb-2" src="${user.avatar_url}"
+                    </div>
+
+                    <div class="col-md-9">
+                        
+                    </div>
+                </div>
+            </div>
+        `;
     }
 }
