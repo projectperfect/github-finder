94840bbee1654f42df547f55018a026c52d30e2d
projectperfect <jprfct@protonmail.com>
2022-09-12 23:36:48 -0500

    getting data from github

---
+++ app.js
@@ -0,0 +1,21 @@
+// Init Github
+const github = new Github;
+
+// Search input 
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+    
+    // Get input text
+    const userText = e.target.value;
+
+    if(userText !== '') {
+        // Make http call
+        GitHub.getUser(userText)
+        .then(data => {
+            console.log(data);
+        })
+    }
+
+});
